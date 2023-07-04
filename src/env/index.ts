2634f97e4477e824b7f6ea91330e6ee6e1a7cbe4
Leonardo Carvalho <leorrc1@gmail.com>
2023-07-04 13:42:25 -0300

    fix deploy 5

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,6 @@ import { z } from 'zod'
 
 const envSchemaa = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
 })
