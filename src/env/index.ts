e48d7e17508a403564100337e1f6b4faad697b69
Leonardo Carvalho <leorrc1@gmail.com>
2023-07-05 15:54:11 -0300

    fix: deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,8 +1,15 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchemaa = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  DATABASE_URL: z.string(),
   JWT_SECRET: z.string(),
   PORT: z.coerce.number().default(3333),
 })
